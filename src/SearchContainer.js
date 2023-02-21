1a2ae34513d1f9107e09679c9f04ff3d389ba4dc
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-20 16:59:50 -0800

    search request/response working

---
+++ src/SearchContainer.js
@@ -0,0 +1,65 @@
+import React from "react";
+import SearchResult from "./SearchResult";
+import SearchBar from "./SearchBar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import axios from 'axios';
+
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+
+class SearchContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      query: '',
+      results: [],
+      error: null,
+    }
+  }
+
+  // updates state when search bar is changed
+  handleInputChange = (event) => {
+    this.setState({query: event.target.value});
+  }
+
+  // submits a request to locationiq for using the search term stored in this.state.query
+  handleSubmitQuery = async (event) => {
+    event.preventDefault();
+
+    try {
+      let request = {
+        url: `https://us1.locationiq.com/v1/search?key=${ACCESS_TOKEN}&q=${this.state.query}&format=json`,
+        method: 'GET'
+      }
+
+      let response = await axios(request);
+
+      this.setState({
+        results: response.data
+      })
+    } catch (err) {
+      console.log(err);
+      this.setState({error: err})
+    }
+  }
+
+  render(){
+    return (
+      <>
+        <Container style={{margin: '3rem 0'}}> 
+          <Row>
+            <SearchBar handleInputChange={this.handleInputChange} handleSubmitQuery={this.handleSubmitQuery} />
+          </Row>
+        </Container>
+        <Container style={{margin: '3rem 0'}}>
+          <Row style={{justifyContent: 'center'}}>
+            {this.state.results.map((result) => <SearchResult name={result.display_name} lat={result.lat} lon={result.lon} />)}
+          </Row>
+        </Container>
+      </>
+    )
+  }
+}
+
+export default SearchContainer;
